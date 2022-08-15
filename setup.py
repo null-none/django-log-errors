4de3e3b7d170f017c192995e846e867e2f1c186b
Dmitry Kalinin <kalinin.mitko@gmail.com>
2022-08-15 23:25:19 +0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="django-log-errors",
+    version="1.0.0",
+    description="Django Log Viewer allows you to read & download log files in the admin page.",
+    url="https://github.com/null-none/django-log-errors",
+    author_email="kalinin.mitko@gmail.com",
+)
